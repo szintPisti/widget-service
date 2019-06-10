e4de7dbc5e2f7719de6ac4efe2f1fff222b29adb
szintPisti <sz.pisti1@gmail.com>
2019-06-10 14:42:41 +0200

    Added project structure and Application class

---
+++ src/main/java/com/miro/widgetservice/WidgetServiceApp.java
@@ -0,0 +1,13 @@
+package com.miro.widgetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WidgetServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WidgetServiceApp.class, args);
+    }
+
+}
